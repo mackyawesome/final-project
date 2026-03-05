9e17a658f714e13f0cba9845a0d122d67d4b1222
mfletcher <mfletcher@stsaviour.org>
2026-03-05 12:29:42 -0500

    idk

--- recipe.py
+++ recipe.py
@@ -1,3 +1,10 @@
+recipes = {
+    "brownies": {"sugar”, “flour”, “butter”, “eggs”, “cocoa powder”, “vanilla extract”, “baking powder”, “salt”},
+    "confetti cake": {“flour”, “sugar”, “butter”, “eggs”, “buttermilk”, “baking powder”, “vanilla extract”, “rainbow sprinkles”},
+    "apple pie": {“apples”, “sugar”, “flour”, “cinnamon”, “butter”, “salt”, “cinnamon”, “nutmeg”, “lemon juice”, “eggs”},
+    "churros": {“butter”, “flour”, “salt”, “sugar”, “eggs”, “vanilla extract”, “oil”, “cinnamon”},
+    "ice cream": {“milk”, “cream”, “sugar”, “vanilla”, “salt”},
+    "cheesecake": {“graham crackers”, “sugar”, “butter”, “cream cheese”, “eggs”, “vanilla extract”, “lemon juice”}
+}
 
-if __name__ == '__main__':
-    print('welcome to my recipe app!')
+
